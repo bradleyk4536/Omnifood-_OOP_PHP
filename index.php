180120745ae68b31ab999ea8e5686fb5dee4663f
Kenneth Bradley <k_bnet570@verizon.net>
2016-06-14 07:19:20 -0400

    Initial setup of repository - template

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html" charset="utf-8">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Omnifoods</title>
+		<!--	BOOTSTRAP -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!--	FONT AWESOME ICONS-->
+		<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
+<!--		ION ICON-->
+		<link rel="stylesheet" href="assets/css/ionicons-2.0.1/css/ionicons.min.css">
+<!--	 GOOGLE FONTS-->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+<!--	 CUSTOM STYLING-->
+		<link rel="stylesheet" href="assets/css/custom.css">
+		 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		 <!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		 <![endif]-->
+	</head>
+<body>
+<header class="site-header" role="banner">
+<!--	NAVBAR-->
+	<div class="navbar-wraper">
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		  <div class="container-fluid">
+			 <!-- Brand and toggle get grouped for better mobile display -->
+			 <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><i class="ion-android-restaurant section_icon"></i> Omnifoods</a>
+			 </div>
+			 <!-- Collect the nav links, forms, and other content for toggling -->
+			 <div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+				  <li class="active"><a href="#">Home</a></li>
+				  <li><a href="resources.html">Resources</a></li>
+				  <li><a href="blog.html">Blog</a></li>
+				  <li><a href="contact.html">Signup</a></li>
+				</ul>
+			 </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+</header>
+	<section id="hero" data-type="background" data-speed="5">
+		<div class="container clear-fix">
+			<div class="row">
+				<div class="col-sm-5">
+					<img src="assets/images/logo.png" alt="">
+				</div>
+				<div class="hero-text col-sm-7">
+				<h1>Goodbye Junk Food</h1>
+				<h2>Hello Super Health Meals.</h2>
+				<a href="#" class="btn btn-full">I&#39;m hungry</a>
+				<a href="#" class="btn btn-ghost">Show Me More</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="get_food_fast">
+		<div class="container">
+			<div class="section_header">
+				<i class="ion-android-restaurant section_icon"></i>
+				<h2>Get Food Fast &mdash; Not Fast Food</h2>
+				<p class="section_intro_text">Hello, were Omnifood, your new premium food delivery serice. We know you&#39;re alway&#39;s busy. No time for cooking. So let us take care of that for you. We are really good at it, we promise you.</p>
+			</div>
+			<div class="row">
+				<div class="col-sm-3">
+				<i class="ion-ios-calendar-outline icon-big" aria-hidden="true"></i>
+				<h3>Up To 365 Days/Year</h3>
+				<p>Never cook again! We really mean that. Our subscription plans include up to 365 day/year coverage. You can also choose to order more flexibly if that&#39;s you style.</p>
+				</div>
+				<div class="col-sm-3">
+				<i class="ion-ios-clock-outline icon-big" aria-hidden="true"></i>
+				<h3>Ready in 20 Minutes</h3>
+				<p>You&#39;re only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you&#39;re 100&#37; happy.</p>
+				</div>
+				<div class="col-sm-3">
+				<i class="ion-ios-nutrition-outline icon-big"></i>
+				<h3>100&#37; Organic</h3>
+				<p>All our vegetables are fresh, organic and locally grown. Animals are raised without added hormones or antibiotics. Good for you health, the environment, and it also tastes better&#33;</p>
+				</div>
+				<div class="col-sm-3">
+				<i class="ion-ios-cart-outline icon-big"></i>
+				<h3>Order Anything</h3>
+				<p>We don&#39;t limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It&#39;s up to you&#33;</p>
+				</div>
+			</div><!--end row-->
+		</div><!--end container-->
+		<div id="menu_button" class="text-center">
+			<a href="" class="btn btn-info btn-lg">See our menu</a>
+		</div>
+	</section>
+	<section id="how_it_works">
+		<div class="container">
+			<div class="section_header">
+				<h2>How It Works &mdash; Simple as 1, 2, 3</h2>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<img src="assets/images/app-iPhone.png" alt="" class="app-screen js--wp-2">
+				</div>
+				<div class="col-sm-6">
+					<div class="work-steps clearfix">
+					<span>1.</span>
+					<p>Choose you subscription plan that best fits your needs and sign up today.</p>
+					</div>
+					<div class="work-steps clearfix">
+					<span>2.</span>
+					<p>Order you delicious meal using our mobile app or website. You can even go old school and call us&#33;</p>
+					</div>
+					<div class="work-steps clearfix">
+					<span>3.</span>
+					<p>Enjoy you meal after less than 20 minutes. See you next time &#33;</p>
+					</div>
+					<a href="#" class="btn-app"><img src="assets/images/download-app-android.png" alt="Google Play"></a>
+					<a href="#" class="btn-app"><img src="assets/images/download-app.svg" alt="Apple Store"></a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="cities">
+		<div class="container">
+			<div class="section_header">
+				<h2>Were Currently In These Cities</h2>
+			</div>
+			<div class="row">
+					<div class="col-sm-3">
+						<img src="assets/images/lisbon-3.jpg" alt="Lisbon">
+						<h3>LISBON</h3>
+						<div class="city-feature">
+							<i class="ion-ios-people-outline icon-small"></i>1600&#43; happy eaters
+						</div>
+						<div class="city-feature">
+							<i class="ion-android-restaurant icon-small"></i>60&#43; top chefs
+						</div>
+						<div class="city-feature">
+							<i class="ion-social-twitter-outline icon-small"></i>
+							<a href="#" class="fmt-link">@omnifood_1x</a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<img src="assets/images/san-francisco.jpg" alt="San Francisco">
+						<h3>San Francisco</h3>
+						<div class="city-feature">
+							<i class="ion-ios-people-outline icon-small"></i>1600&#43; happy eaters
+						</div>
+						<div class="city-feature">
+							<i class="ion-android-restaurant icon-small"></i>160&#43; top chefs
+						</div>
+						<div class="city-feature">
+							<i class="ion-social-twitter-outline icon-small"></i>
+							<a href="#" class="fmt-link">@omnifood_sf</a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<img src="assets/images/berlin.jpg" alt="Berlin">
+						<h3>Berlin</h3>
+						<div class="city-feature">
+							<i class="ion-ios-people-outline icon-small"></i>1600&#43; happy eaters
+						</div>
+						<div class="city-feature">
+							<i class="ion-android-restaurant icon-small"></i>110&#43; top chefs
+						</div>
+						<div class="city-feature">
+							<i class="ion-social-twitter-outline icon-small"></i>
+							<a href="#" class="fmt-link">@omnifood_berlin</a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<img src="assets/images/london.jpg" alt="London">
+						<h3>London</h3>
+						<div class="city-feature">
+							<i class="ion-ios-people-outline icon-small"></i>1600&#43; happy eaters
+						</div>
+						<div class="city-feature">
+							<i class="ion-android-restaurant icon-small"></i>50&#43; top chefs
+						</div>
+						<div class="city-feature">
+							<i class="ion-social-twitter-outline icon-small"></i>
+							<a href="#" class="fmt-link">@omnifood_london</a>
+						</div>
+					</div>
+				</div>
+		</div>
+	</section>
+	<section id="testimonial">
+		<div class="container">
+			<div class="section_header">
+				<h2>Our Customers Can&#39;t Live Without Us</h2>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<blockquote>Omnifood is just awesome I just launched a startup which leaves me with no time for cooking, so Ominfood is a life&#45;saver. Now that I got used to it, I couldn&#39;t live without it.
+						<cite><img src="assets/images/customer-1.jpg" alt="Alberto Duncan"> Alberto Duncan</cite>
+					</blockquote>
+				</div>
+				<div class="col-sm-4">
+					<blockquote>Inexpensive, healthy and great-tasting meals, delivered right to my home. We have lots of food delivery here in Lisbon, but no one comes even close to Omnifood.
+						<cite><img src="assets/images/customer-2.jpg" alt="Joana Silva"> Joana Silva</cite>
+					</blockquote>
+				</div>
+				<div class="col-sm-4">
+					<blockquote>Inexpensive, healthy and great-tasting meals, delivered right to my home. We have lots of food delivery here in Lisbon, but no one comes even close to Omnifood.
+						<cite><img src="assets/images/customer-3.jpg" alt="Milton Chapman">Milton Chapman</cite>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="call_to_action">
+		<div class="container">
+			<div class="section_header">
+				<h2>Start Eating Healthy Today</h2>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<div>
+						<h3>Premium</h3>
+						<p><span>&#36;399</span> / month</p>
+						<p>That&#39;s only &#36;13.30 per meal.</p>
+					</div>
+					<div>
+						<ul>
+							<li><i class="ion-checkmark"></i>1 meal every day</li>
+							<li><i class="ion-checkmark"></i>Order 24/7</li>
+							<li><i class="ion-checkmark"></i>Access to newest creations</li>
+							<li><i class="ion-checkmark"></i>Free delivery</li>
+						</ul>
+					</div>
+					<div><a href="#" class="btn btn-success btn-lg">Sign up now</a></div>
+				</div>
+				<div class="col-sm-4">
+					<div>
+						<h3>Premium</h3>
+						<p><span>&#36;399</span> / month</p>
+						<p>That&#39;s only &#36;13.30 per meal.</p>
+					</div>
+					<div>
+						<ul>
+							<li><i class="ion-checkmark"></i>1 meal every day</li>
+							<li><i class="ion-checkmark"></i>Order 24/7</li>
+							<li><i class="ion-checkmark"></i>Access to newest creations</li>
+							<li><i class="ion-checkmark"></i>Free delivery</li>
+						</ul>
+					</div>
+					<div><a href="#" class="btn btn-success btn-lg">Sign up now</a></div>
+				</div>
+				<div class="col-sm-4">
+					<div>
+						<h3>Premium</h3>
+						<p><span>&#36;399</span> / month</p>
+						<p>That&#39;s only &#36;13.30 per meal.</p>
+					</div>
+					<div>
+						<ul>
+							<li><i class="ion-checkmark"></i>1 meal every day</li>
+							<li><i class="ion-checkmark"></i>Order 24/7</li>
+							<li><i class="ion-checkmark"></i>Access to newest creations</li>
+							<li><i class="ion-checkmark"></i>Free delivery</li>
+						</ul>
+					</div>
+					<div><a href="#" class="btn btn-success btn-lg">Sign up now</a></div>
+				</div>
+			</div>
+		</div>
+	</section>
+		<!--Core javascript files for bootstrap-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/jquery-2.1.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+	</body>
+</html>
+
+
+
