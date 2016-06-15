67dc024c25112a2792da88dcf9760af7a1d8b136
Kenneth Bradley <k_bnet570@verizon.net>
2016-06-15 06:11:18 -0400

    Added admin dashboard skeleton to application

---
+++ admin/includes/admin_header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html" charset="utf-8">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Omnifoods</title>
+		<!--	BOOTSTRAP -->
+		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+<!--	FONT AWESOME ICONS-->
+		<link rel="stylesheet" href="../assets/css/font-awesome/css/font-awesome.min.css">
+<!--		ION ICON-->
+		<link rel="stylesheet" href="../assets/css/ionicons-2.0.1/css/ionicons.min.css">
+<!--	 GOOGLE FONTS-->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+<!--	 CUSTOM STYLING-->
+	 <link href="css/sb-admin.css" rel="stylesheet">
+		 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		 <!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		 <![endif]-->
+	</head>
+<body>
---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<?php include "includes/admin_header.php"; ?>
+ <div id="wrapper">
+	<?php include "includes/admin_top_navigation.php"; ?>
+	<?php include "includes/admin_footer.php"; ?>
+  <div id="page-wrapper">
+		<div class="container-fluid">
+			 <!-- Page Heading -->
+			 <div class="row">
+				  <div class="col-lg-12">
+						<h1 class="page-header">Omnifood</h1>
+						<ol class="breadcrumb">
+							 <li>
+								  <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+							 </li>
+							 <li class="active">
+								  <i class="fa fa-file"></i> Blank Page
+							 </li>
+						</ol>
+				  </div>
+			 </div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+		<hr>
+		<?php include "includes/admin_dashboard.php"; ?>
+  </div><!-- /#page-wrapper -->
+</div>
+    <!-- /#wrapper -->
